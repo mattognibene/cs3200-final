bd572f444274b0220ed92ea435344836a87d7413
Kendall <kkmeenan@gmail.com>
2020-11-30 18:50:49 -0500

    Not working :(

--- app/recipe-app/src/App.js
+++ app/recipe-app/src/App.js
@@ -1,39 +1,34 @@
-import React, { useState, useRef } from 'react';
+import { React, useState, useRef } from 'react';
 //import logo from './logo.svg'
-import IngList from './IngList'
 //import './App.css';
 
 
 function App() {
-  const [ings, setIngs] = useState([])
-  const ingRef = useRef()
+  //const [ings, setIngs] = useState([])
+  //const ingRef = useRef()
 
-  function handleIng(e) {
-	const name = ingRef.current.value
-	if (name === '') return
-	setIngs(prevIngs => {
-		return [...prevIngs, {name: name}]
-	})
-	ingRef.current.value = null;
-  } 
+  //function handleIng(e) {
+	//const name = ingRef.current.value
+	//if (name === '') return
+	//setIngs(prevIngs => {
+	//	return [...prevIngs, {name: name}]
+	//})
+	//ingRef.current.value = null;
+  //} 
 
   return(
-	<div className="App">
-		<header className="App-header">
-			<h1 className="App Title">Welcome to our Recipe Generator</h1>
-		</header>
-		<input ref={ingRef} type="text" />
-		<button onClick={handleIng}>Enter ingredient</button>
-		<IngList ings={ings} />
-		<button>Get recipes</button>
-		<r1 className="Recipe1">
-			Cookies and Cream Fudge
-		</r1>
-		<r2 className="Recipe2">
-			Peanut Butter Fudge
-		</r2>
-	</div>
-     );
+	<>
+		<h1>Recipe Generator</h1>
+		<input type="text" />
+		<button>Enter ingredients</button>
+		<e1>
+			Ingredients Entered: 
+		</e1>
+		<e2> Stuffing, Mashed Potatoes, Eggs </e2>
+		<button>Get Recipes</button>
+		<e3>Recipes Found: Stuffing Bites</e3>
+	</>
+     )
 }
 
 export default App;
