d5703f946dc2c0608c72be75fa4928d201575f8d
Kendall <kkmeenan@gmail.com>
2020-11-30 00:22:01 -0500

    bare bones main screen

--- app/recipe-app/src/App.js
+++ app/recipe-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from 'react';
+//import logo from './logo.svg'
+import IngList from './IngList'
+//import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ings, setIngs] = useState([])
+  const ingRef = useRef()
+
+  function handleIng(e) {
+	const name = ingRef.current.value
+	if (name === '') return
+	setIngs(prevIngs => {
+		return [...prevIngs, {name: name}]
+	})
+	ingRef.current.value = null;
+  } 
+
+  return(
+	<div className="App">
+		<header className="App-header">
+			<h1 className="App Title">Welcome to our Recipe Generator</h1>
+		</header>
+		<input ref={ingRef} type="text" />
+		<button onClick={handleIng}>Enter ingredient</button>
+		<IngList ings={ings} />
+		<button>Get recipes</button>
+		<r1 className="Recipe1">
+			Cookies and Cream Fudge
+		</r1>
+		<r2 className="Recipe2">
+			Peanut Butter Fudge
+		</r2>
+	</div>
+     );
 }
 
 export default App;
---
+++ app/recipe-app/src/Ing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import './Ing.css'
+
+function Ing(props) {
+  return (
+	<div className="ingredient">
+		<span>{props.name}</span>
+	</div>
+  )
+}
+
+export default Ing;
